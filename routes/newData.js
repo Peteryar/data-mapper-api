63ca391793fcc440b823f0917b7e1232ae29dce2
Agent Peter <peteryar19@gmail.com>
2020-04-26 11:13:18 +0100

    code refactor

--- routes/newData.js
+++ routes/newData.js
@@ -43,47 +43,4 @@ router.get('/', asyncMiddleware(async (req, res) => {
     res.send(dataProviders)
 }))
 
-router.get('/:providerId', asyncMiddleware(async (req, res) => {
-    const dataProvider = await DataProvider.findOne({
-        providerId: req.params.providerId,
-    })
-
-    if (!dataProvider) return res.status(400).send("providerId not found");
-
-    const data = {}
-    let counter = 0;
-    
-
-    const getData = dataProvider.data.forEach(element => {
-        if (element.name.toLowerCase() === req.query.name.toLowerCase())
-            data.name = element.name;
-        if (String(element.age) === req.query.age)
-            data.age = element.age;
-        if (String(element.timestamp) === req.query.timestamp)
-            data.timestamp = element.timestamp;
-    });
-
-    function analyseData() {
-        if (data.name) counter += 1;
-        if (data.age) counter += 1;
-        if (data.timestamp) counter += 1
-
-        if (counter === 3) {
-            return data
-        } else {
-            return 'query parameters dont match any document, please check and try again!!'
-        }
-
-    }
-
-    
-    
-
-    const result = analyseData()
-
-    res.send(result)
-})
-
-)
-
 module.exports = router;
--- routes/searchData.js
+++ routes/searchData.js
@@ -1,28 +1,42 @@
 const express = require('express');
-const { DataProvider, validateData, Data } = require('../dataModel');
+const { DataProvider } = require('../dataModel');
 const asyncMiddleware = require('../middlewares/asyncMiddleware')
 
 const router = express.Router();
-
-router.get('/', asyncMiddleware(async (req, res) => {
-    const dataProviders = await DataProvider.find()
-        .select("-_id -data._id")
-
-    res.send(dataProviders)
-}))
 
 router.get('/:providerId', asyncMiddleware(async (req, res) => {
     const dataProvider = await DataProvider.findOne({
         providerId: req.params.providerId,
     })
+    .select("-_id -data._id -__v")
+
+
 
     if (!dataProvider) return res.status(400).send("providerId not found");
 
+    let result;
+    let queryCounter = 0;
+
+    for (x in req.query) {
+        queryCounter += 1
+    }
+    if (queryCounter === 0)
+        result = dataProvider;
+    if (queryCounter > 0 && queryCounter < 3)
+        result = "Please provide all search requirement for data such as name, age and timestamp accurately!!"
+
+    if(queryCounter === 3)result = analyseQueryData(dataProvider.data, req);
+
+    res.send(result)
+})
+
+)
+
+function analyseQueryData(queryData, req) {
     const data = {}
     let counter = 0;
-    
 
-    const getData = dataProvider.data.forEach(element => {
+    const getData = queryData.forEach(element => {
         if (element.name.toLowerCase() === req.query.name.toLowerCase())
             data.name = element.name;
         if (String(element.age) === req.query.age)
@@ -31,27 +45,18 @@ router.get('/:providerId', asyncMiddleware(async (req, res) => {
             data.timestamp = element.timestamp;
     });
 
-    function analyseData() {
-        if (data.name) counter += 1;
-        if (data.age) counter += 1;
-        if (data.timestamp) counter += 1
+    if (data.name) counter += 1;
+    if (data.age) counter += 1;
+    if (data.timestamp) counter += 1
 
-        if (counter === 3) {
-            return data
-        } else {
-            return 'query parameters dont match any document, please check and try again!!'
-        }
-
+    if (counter === 3) {
+        return data
+    } else {
+        return 'query parameters dont match any document, please check and try again!!'
     }
 
-    
-    
+}
 
-    const result = analyseData()
 
-    res.send(result)
-})
-
-)
 
 module.exports = router;
